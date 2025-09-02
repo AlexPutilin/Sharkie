e3a4bd6ad1b3fb6d7f7ed8e04af83bd10921051a
Alexander Putilin <alexander.putilin@yahoo.de>
2025-09-02 12:33:37 +0200

    refactor: Background class

--- js/classes/Background.js
+++ js/classes/Background.js
@@ -1,7 +1,17 @@
-class Background extends Actor {
+class Background {
+    posX;
+    posY = 0;
+    width = 1080;
+    height = 720;
+    collisionBox = {x: 0, y: 0, w: 0, h: 0};
+    img = new Image();
+
     constructor(imgPath, x) {
-        super(x, 0, 1080, 720);
         this.loadImg(imgPath);
+        this.posX = x;
     }
-    
+
+    loadImg(path) {
+        this.img.src = path;
+    }
 }
