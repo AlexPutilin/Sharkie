b473016d2a7ba5ba7850ac5d6d7f8cf9d95588eb
Alexander Putilin <alexander.putilin@yahoo.de>
2025-09-25 19:33:33 +0200

    Add files via upload

--- js/templates.js
+++ js/templates.js
@@ -17,7 +17,7 @@ function getControlsTemplate() {
             </div>
             <div class="d-row-space">
                 <div class="key-wrapper">
-                    <div class="key" style="width: 176px;">Spacebar</div>
+                    <div class="key" style="width: 208px;">Spacebar</div>
                 </div>
                 <span>Attack</span>
             </div>
@@ -29,11 +29,12 @@ function getControlsTemplate() {
 function getLegalNoticeTemplate() {
     return `
         <h2>Legal Notice</h2>
-        <span>Information according to § 5 TMG (German Telemedia Act):</span>
-        <span>Alexander Putilin</span>
-        <span>Germany</span>
+        <span>Information according to § 5 TMG (German Telemedia Act):</span><br>
         <br>
-        <span>Email: alexander.putilin@yahoo.de</span>
+        <span>Alexander Putilin</span><br>
+        <span>Germany</span><br>
+        <br>
+        <span>Email: alexander.putilin@yahoo.de</span><br>
         <br>
         <p>
             Responsible for the content according to § 55 Abs. 2 RStV:<br>
@@ -47,12 +48,14 @@ function getPrivacyPolicyTemplate() {
     return `
         <h2>Privacy Policy</h2>
         <span>This website only provides access to a small browser game.</span>
+        <br><br>
         <table>
-            <ul>No personal data is collected, stored, or processed.</ul>
-            <ul>No cookies are used.</ul>
-            <ul>No tracking (e.g., Google Analytics) is implemented.</ul>
-            <ul>No server logs with personal data are analyzed.</ul>
+            <ul>- No personal data is collected, stored, or processed.</ul>
+            <ul>- No cookies are used.</ul>
+            <ul>- No tracking (e.g., Google Analytics) is implemented.</ul>
+            <ul>- No server logs with personal data are analyzed.</ul>
         </table>
+        <br>
         <span>The game can be played completely anonymously.</span>
     `
 }
